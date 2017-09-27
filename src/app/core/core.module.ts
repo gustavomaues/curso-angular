d3d0ea06f44099849838574841e698ebf14f6ef5
Gustavo Maués de Oliveira Lobato <gustavomaues@ufpa.br>
2017-09-26 23:36:28 -0300

    Adicionando o componente de rodapé

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  declarations: [TopoComponent],
-  exports: [TopoComponent]
+  declarations: [TopoComponent, RodapeComponent],
+  exports: [TopoComponent, RodapeComponent]
 })
 export class CoreModule { }
