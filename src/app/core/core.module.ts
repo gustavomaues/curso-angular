581be96e23306b734148934962d7a06a08f49e79
Gustavo Maués de Oliveira Lobato <gustavomaues@ufpa.br>
2017-09-26 23:50:25 -0300

    Adicionando o componente da tela inicial

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  declarations: [TopoComponent, RodapeComponent],
-  exports: [TopoComponent, RodapeComponent]
+  declarations: [TopoComponent, RodapeComponent, HomeComponent],
+  exports: [TopoComponent, RodapeComponent, HomeComponent]
 })
 export class CoreModule { }
