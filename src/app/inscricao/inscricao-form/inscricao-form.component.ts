4e42b1220949ba7ca09499874f0ced442a91e8a1
Gustavo Maués de Oliveira Lobato <gustavomaues@ufpa.br>
2017-09-27 02:28:45 -0300

    Adicionando o componente do formulário de inscrição

---
+++ src/app/inscricao/inscricao-form/inscricao-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inscricao-form',
+  templateUrl: './inscricao-form.component.html',
+  styleUrls: ['./inscricao-form.component.css']
+})
+export class InscricaoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/inscricao/inscricao.module.ts
+++ src/app/inscricao/inscricao.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { InscricaoRoutingModule } from './inscricao-routing.module';
 import { CursoListaComponent } from './curso-lista/curso-lista.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { InscricaoComponent } from './inscricao/inscricao.component';
+import { InscricaoFormComponent } from './inscricao-form/inscricao-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     InscricaoRoutingModule
   ],
-  declarations: [CursoListaComponent, CursoDetalheComponent]
+  declarations: [CursoListaComponent, CursoDetalheComponent, InscricaoComponent, InscricaoFormComponent]
 })
 export class InscricaoModule { }
