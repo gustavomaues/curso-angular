7e45e0ea40921cb03927f074de710dab0fbb84bf
Gustavo Maués de Oliveira Lobato <gustavomaues@ufpa.br>
2017-09-26 22:48:56 -0300

    criando o componente core/topo

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { TopoComponent } from './topo/topo.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  declarations: []
+  declarations: [TopoComponent]
 })
 export class CoreModule { }
